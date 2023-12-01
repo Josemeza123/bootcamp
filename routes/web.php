6399112b894d7236a4547bbccbcebd80e6a8d112
Jose <enriquesais123@gmail.com>
2023-11-30 23:41:13 -0500

    actualizacion

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+
+// DB::listen(function ($query) {
+//     dump($query->sql);
+// });
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +24,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/chirps', [ChirpController::class, 'index'])
+        ->name('chirps.index');
+
+    Route::post('/chirps', [ChirpController::class, 'store'])
+        ->name('chrips.store');
+
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit'])
+        ->name('chirps.edit');
+
+    Route::put('/chirps/{chirp}', [ChirpController::class, 'update'])
+        ->name('chirps.update');
+
+    Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy'])
+        ->name('chirps.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
